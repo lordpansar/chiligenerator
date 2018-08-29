2a70c83530dc16cde8dcd0c5d8e61a0afdd17663
Magnus Sundström <kmvsundstrom@gmail.com>
2018-08-29 16:25:16 +0200

    Added data sources

---
+++ scripts/data.js
@@ -0,0 +1,10 @@
+let places = ['Detroit', 'Kabul', 'Kingston', 'S:t Louis', 'Lagos', 'New Orleans', 'Baghdad', 'Guatemala City', 'Baltimore',
+    'Tijuana', 'Acapulco', 'Johannesburg', 'Caracas', 'Chicago', 'Oakland', 'Cape Town', 'Karachi', 'Kiev', 'Nairobi',
+    'Luanda', 'Benghazi', 'Damascus', 'Chernobyl', 'Port-au-Prince', 'Manila', 'Fukushima', 'Compton', 'Bronx', 'Nagoya',
+    'Jakarta', 'Tokyo'];
+
+let animals = ['scorpion', 'tiger', 'viper', 'anaconda', 'constrictor', 'black widow', 'hippo', 'great white', 'mamba', 'grizzly', //10
+    'lion', 'box jellyfish', 'taranchula', 'funnel web spider', 'Komodo dragon', 'mosquito', 'tick', 'elephant', 'crocodile', //9
+    'alligator', 'gorilla', 'wolf', 'moose', 'tiger shark', 'rattle snake', 'chupacabra', 'werewolf', 'rabid dog', 'dart frog',
+    'dragon', 'fire ant'];
+
