8323a67577923d12dce0da476ee1f9fbe1d00e96
Magnus Sundström <kmvsundstrom@gmail.com>
2018-08-30 09:11:46 +0200

    Updated data lists

--- scripts/data.js
+++ scripts/data.js
@@ -1,10 +1,9 @@
 let places = ['Detroit', 'Kabul', 'Kingston', 'S:t Louis', 'Lagos', 'New Orleans', 'Baghdad', 'Guatemala City', 'Baltimore',
     'Tijuana', 'Acapulco', 'Johannesburg', 'Caracas', 'Chicago', 'Oakland', 'Cape Town', 'Karachi', 'Kiev', 'Nairobi',
     'Luanda', 'Benghazi', 'Damascus', 'Chernobyl', 'Port-au-Prince', 'Manila', 'Fukushima', 'Compton', 'Bronx', 'Nagoya',
-    'Jakarta', 'Tokyo'];
+    'Jakarta', 'Tokyo', 'Aleppo', 'Tripoli'];
 
-let animals = ['scorpion', 'tiger', 'viper', 'anaconda', 'constrictor', 'black widow', 'hippo', 'great white', 'mamba', 'grizzly', //10
-    'lion', 'box jellyfish', 'taranchula', 'funnel web spider', 'Komodo dragon', 'mosquito', 'tick', 'elephant', 'crocodile', //9
+let animals = ['scorpion', 'tiger', 'viper', 'anaconda', 'constrictor', 'black widow', 'hippo', 'great white', 'mamba', 'grizzly',
+    'lion', 'box jellyfish', 'taranchula', 'funnel web spider', 'Komodo dragon', 'mosquito', 'tick', 'elephant', 'crocodile',
     'alligator', 'gorilla', 'wolf', 'moose', 'tiger shark', 'rattle snake', 'chupacabra', 'werewolf', 'rabid dog', 'dart frog',
-    'dragon', 'fire ant'];
-
+    'dragon', 'fire ant', 'cobra', 'cougar'];
